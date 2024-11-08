f96958eae83cc930ae5852a82d47a3966a46a1be
batigunemrah <batigunemrah@gmail.com>
2024-11-08 23:03:29 +0300

    Add admin configs

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import File, FileAccessLog
 
-# Register your models here.
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'user', 'file_size', 'upload_timestamp', 'is_encrypted')
+    search_fields = ('filename', 'user__username')
+    list_filter = ('is_encrypted', 'upload_timestamp')
+
+@admin.register(FileAccessLog)
+class FileAccessLogAdmin(admin.ModelAdmin):
+    list_display = ('file', 'user', 'access_timestamp', 'access_type')
+    search_fields = ('file__filename', 'user__username')
+    list_filter = ('access_type', 'access_timestamp')
